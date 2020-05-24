1f50988a0264f0b2bead1fa1bf93d55d4dd020c4
simon-donike <simon-donike@users.noreply.github.com>
2020-05-24 14:12:26 +0200

    jupyter changes

--- data/testing.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("linc_seg.csv",delimiter=";")
-df_array = np.asarray(df)
-flat_list = [item for sublist in df_array for item in sublist]
-print(flat_list)
